d3dc4e452d32b70f9f368076c7e3c9290a0b20b9
Scheals <95084627+scheals@users.noreply.github.com>
2023-01-08 21:22:43 +0100

    fix: change routes to be more sane

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@
 
 Rails.application.routes.draw do
   get "flights/", to: "flights#find_flights"
-  get "flights/bookings/new", to: "bookings#new"
-  post "flights/bookings", to: "bookings#create"
-  resources :bookings, only: :show
+  resources :bookings, only: %i[new create show]
   resources :flights, only: :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   root "flights#index"
 end
