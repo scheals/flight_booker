9c8635aa7fab20c1922f135374f0a6c696f31da6
Scheals <95084627+scheals@users.noreply.github.com>
2023-01-09 16:11:30 +0100

    fix: make flight searching more sane

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get "flights/", to: "flights#find_flights"
   resources :bookings, only: %i[new create show]
   resources :flights, only: :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
