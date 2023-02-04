8aa3d3a628849e36bda3fe88bd716cf04c93626e
scheals <95084627+scheals@users.noreply.github.com>
2023-02-04 13:07:21 +0100

    feat: add removing passengers from booking form

--- app/javascript/controllers/passengers_controller.js
+++ app/javascript/controllers/passengers_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ 'passenger' ]
-  static values = { amount: Number }
+  static values = { amount: Number, index: Number }
 
   connect() {
   }
@@ -14,28 +14,38 @@ export default class extends Controller {
     this.addPassengerButton.before(this.preparePassenger())
   }
 
+  remove(event) {
+    event.preventDefault()
+    if (this.passengerTargets.length == 1) return console.log("Can't get rid of the last one!")
+    this.amountValue--
+    this.deletedPassenger = event.target.parentNode
+    this.startingIndex = this.passengerTargets.findIndex((target) => target === this.deletedPassenger) + 1
+    this.passengersToUpdate = this.passengerTargets.slice(this.startingIndex, this.passengerTargets.length)
+    this.deletedPassenger.remove()
+    this.passengersToUpdate.forEach((passenger) => this.preparePassenger(passenger, this.startingIndex++))
+  }
+
   clonePassengerTemplate() {
     this.passengerFormTemplate = document.querySelector('#passengerTemplate')
     this.clone = this.passengerFormTemplate.content.cloneNode(true)
     return this.clone
   }
 
-  preparePassenger() {
-    this.templateClone = this.clonePassengerTemplate()
-    this.setPassengerHeader(this.templateClone, this.amountValue)
-    this.setPassengerAttributes(this.templateClone, this.amountValue - 1)
-    return this.templateClone
+  preparePassenger(passenger = this.clonePassengerTemplate(), passengerNumber = this.amountValue) {
+    this.setPassengerHeader(passenger, passengerNumber)
+    this.setPassengerAttributes(passenger, passengerNumber - 1)
+    return passenger
   }
 
-  setPassengerHeader(element, passengerNumber) {
-    this.header = element.querySelector('h3')
+  setPassengerHeader(passenger, passengerNumber) {
+    this.header = passenger.querySelector('h3')
     this.header.textContent = `Passenger ${passengerNumber}`
-    return element
+    return passenger
   }
 
-  setPassengerAttributes(element, paramIndex) {
-    this.labels = element.querySelectorAll('div>div>label')
-    this.inputs = element.querySelectorAll('div>div>input')
+  setPassengerAttributes(passenger, paramIndex) {
+    this.labels = passenger.querySelectorAll('div>div>label')
+    this.inputs = passenger.querySelectorAll('div>div>input')
     this.idAttribute = `booking_passengers_attributes_${paramIndex}`
     this.paramAttribute = `booking[passengers_attributes][${paramIndex}]`
     this.labels[0].setAttribute('for', `${paramIndex}_name`)
@@ -44,6 +54,6 @@ export default class extends Controller {
     this.labels[1].setAttribute('for', `${paramIndex}_email`)
     this.inputs[1].setAttribute('id', `${this.idAttribute}_email`)
     this.inputs[1].setAttribute('name', `${this.paramAttribute}[email]`)
-    return element
+    return passenger
   }
 }
